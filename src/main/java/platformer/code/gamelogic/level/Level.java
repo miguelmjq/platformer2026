eaaf53f950ef7d0e7042452b64e26980f7d2914b
miguelmjq <mdjmirabalq@gmail.com>
2026-05-26 15:06:02 +0000

    WATER DONE 5/26/26

--- src/main/java/platformer/code/gamelogic/level/Level.java
+++ src/main/java/platformer/code/gamelogic/level/Level.java
@@ -224,31 +224,39 @@ public class Level {
 		Boolean up1 = row - 1 >= 0;
 		Boolean left1 = col - 1 >= 0;
 		Boolean right1 = col + 1 < map.getTiles().length;
-		Boolean nothingLeft = left1 && !(map.getTiles()[col - 1][row] instanceof Water) && !(map.getTiles()[col - 1][row].isSolid());
-		Boolean nothingRight = right1 && !(map.getTiles()[col + 1][row] instanceof Water) && !(map.getTiles()[col - 1][row].isSolid());
+		Boolean nothingLeft = left1 && !(map.getTiles()[col - 1][row] instanceof Water)
+				&& !(map.getTiles()[col - 1][row].isSolid());
+		Boolean nothingRight = right1 && !(map.getTiles()[col + 1][row] instanceof Water)
+				&& !(map.getTiles()[col + 1][row].isSolid());
 
 		Water w = new Water(col, row, tileSize, tileset.getImage(water), this, fullness);
 		map.addTile(col, row, w);
-		if (down1 && !(map.getTiles()[col][row+1].isSolid())){
-			water(col, row+1, map, 0);
-		}
-		else if (fullness == 0 && map.getTiles()[col][row+1].isSolid()){
-			water(col, row, map, 3);
-		}
-		else if (fullness == 1) {
+		if (down1 && !(map.getTiles()[col][row + 1].isSolid())) {
+
+			if (row + 2 < map.getTiles()[col].length && map.getTiles()[col][row + 2].isSolid()) {
+
+				water(col, row+1, map, 3);
+			} else {
+				water(col, row + 1, map, 0);
+			}
+
+		} else {
+
 			if (nothingLeft) {
-				water(col - 1, row, map, fullness);
+
+				if (fullness > 1)
+					water(col - 1, row, map, fullness - 1);
+				else {
+					water(col - 1, row, map, 1);
+				}
 			}
 			if (nothingRight) {
-				water(col + 1, row, map, fullness);
-			}
-		}
-		else if (fullness > 1) {
-			if (nothingLeft) {
-				water(col - 1, row, map, fullness - 1);
-			}
-			if (nothingRight) {
-				water(col + 1, row, map, fullness - 1);
+
+				if (fullness > 1)
+					water(col + 1, row, map, fullness - 1);
+				else {
+					water(col + 1, row, map, 1);
+				}
 			}
 		}
 	}
